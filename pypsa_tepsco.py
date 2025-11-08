d0e12f60aa70b1f27d5a9584cf0737f62a66cef7
yanase1147 <yanase1147@gmail.com>
2025-11-08 23:49:37 +0900

    ノートブックの実行カウントを修正し、インポート文を整理してコードの可読性を向上

--- pypsa_tepsco.py
+++ pypsa_tepsco.py
@@ -3,15 +3,12 @@
 import os, pandas as pd
 from IPython.display import clear_output, display
 from concurrent.futures import ThreadPoolExecutor
-import warnings
-import requests
-import re
-import shutil
+import warnings, requests, re, shutil
 
 # 需要データの読み込み
 def import_demand_data_from_network_file(network, network_file_name, demand_change_compared_to_2024):
     demand_data_raw = pd.read_excel(network_file_name, sheet_name='Demand', index_col=0, parse_dates=True)
-        
+
     # demand_data_rawに格納されたデータをnetwork.loads_t.p_setに適用
     for load in network.loads.index:
         if load in demand_data_raw.columns:
